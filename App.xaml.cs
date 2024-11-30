bc6cd7070f9287ca99753f9b889284194b19d78e
angeldiazme <106854335+angeldiazme@users.noreply.github.com>
2024-11-29 19:53:51 -0600

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MediaManager;
+
+namespace Examen_Grupo2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+            CrossMediaManager.Current.Init();
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Maps.Handlers;
+using Plugin.Maui.Audio;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace Examen_Grupo2
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
+                .ConfigureMauiHandlers(handlers =>
+                {
+                    handlers.AddHandler(typeof(Map), typeof(MapHandler));
+                })
+                .UseMauiMaps()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            builder.Services.AddSingleton(AudioManager.Current);
+            builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
